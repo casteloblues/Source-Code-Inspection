f34b71e96aabcda8ea7f9d5205c394df2c5d0535
31240550 <31240550@087858-P33S105.macklabs.br>
2015-09-23 10:33:57 -0300

    - Correção do índice i dentro do método inserir (antes era 1, corrigido para i) - Método getTroco desnecessário (marcado como comentário no código)

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,7 +22,7 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
@@ -36,9 +36,9 @@ public class TicketMachine {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
-    }
+//    public Iterator<Integer> getTroco() {   metodo desnecessario
+//        return null;
+//    }
 
     public String imprimir() throws SaldoInsuficienteException {
         if (saldo < valor) {
